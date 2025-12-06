bc9abb304faef14f93ad988bf65d00edc6e2400b
PhetcharatPhrathanee <phetcharatphrathanee@gmail.com>
2025-12-06 17:01:42 +0700

    cd

--- Project/MainForm.Designer.cs
+++ Project/MainForm.Designer.cs
@@ -28,19 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+           // this.button1 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(371, 205);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            //this.button1.Location = new System.Drawing.Point(371, 205);
+           // this.button1.Name = "button1";
+            //this.button1.Size = new System.Drawing.Size(75, 23);
+            //this.button1.TabIndex = 0;
+           // this.button1.Text = "button1";
+            //this.button1.UseVisualStyleBackColor = true;
             // 
             // checkBox2
             // 
@@ -79,6 +79,6 @@
 
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
-        private System.Windows.Forms.CheckBox checkBox1;
+       // private System.Windows.Forms.CheckBox checkBox1;
     }
 }
