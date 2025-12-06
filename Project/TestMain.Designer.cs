65127e07d52ca6b0f583038ab950249fe0f3d8cb
PhetcharatPhrathanee <phetcharatphrathanee@gmail.com>
2025-12-06 19:52:29 +0700

    11

--- Project/TestMain.Designer.cs
+++ Project/TestMain.Designer.cs
@@ -36,7 +36,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1098, 750);
+            this.panel1.Size = new System.Drawing.Size(1664, 1011);
             this.panel1.TabIndex = 0;
             // 
             // TestMain
@@ -45,7 +45,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(1098, 750);
+            this.ClientSize = new System.Drawing.Size(1664, 1011);
             this.Controls.Add(this.panel1);
             this.Name = "TestMain";
             this.Text = "Form1";
